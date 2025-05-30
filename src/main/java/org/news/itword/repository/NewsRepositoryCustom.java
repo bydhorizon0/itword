c6b83a8eeeff9f60fed6059fe81dd6aed88bb6ec
bydhorizon0 <bydhorizon0@gmail.com>
2025-05-30 17:56:16 +0900

    News 페이지네이션

--- src/main/java/org/news/itword/repository/NewsRepositoryCustom.java
+++ src/main/java/org/news/itword/repository/NewsRepositoryCustom.java
@@ -1,7 +1,10 @@
 package org.news.itword.repository;
 
 import org.news.itword.dto.NewsDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface NewsRepositoryCustom {
+    Page<NewsDTO> getNewsList(Pageable pageable);
     NewsDTO getNewsInfo(Long id);
 }
--- src/main/java/org/news/itword/repository/NewsRepositoryCustomImpl.java
+++ src/main/java/org/news/itword/repository/NewsRepositoryCustomImpl.java
@@ -7,12 +7,46 @@ import lombok.extern.log4j.Log4j2;
 import org.news.itword.dto.NewsDTO;
 import org.news.itword.entity.QMember;
 import org.news.itword.entity.QNews;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
 
 @Log4j2
 @RequiredArgsConstructor
 public class NewsRepositoryCustomImpl implements NewsRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Page<NewsDTO> getNewsList(Pageable pageable) {
+        QNews news = QNews.news;
+        QMember member = QMember.member;
+
+        List<NewsDTO> result = queryFactory.select(Projections.constructor(NewsDTO.class,
+                        news.id,
+                        news.title,
+                        news.content,
+                        news.createdAt,
+                        news.updatedAt,
+                        member.email
+                ))
+                .from(news)
+                .join(member).on(news.member.id.eq(member.id))
+                .limit(pageable.getPageSize())
+                .offset(pageable.getOffset())
+                .fetch();
+
+        Long total = Optional.ofNullable(
+                queryFactory.select(news.count())
+                        .from(news)
+                        .fetchOne()
+        ).orElseThrow(() -> new IllegalStateException("개수 조회 실패"));
+
+    return new PageImpl<>(result, pageable, total);
+    }
 
     @Override
     public NewsDTO getNewsInfo(Long id) {
