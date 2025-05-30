9904e71131c35e16f4879597a3be419f43fb7d6a
bydhorizon0 <bydhorizon0@gmail.com>
2025-05-30 17:55:56 +0900

    PageRequestDTO 추가

--- src/main/java/org/news/itword/dto/PageRequestDTO.java
+++ src/main/java/org/news/itword/dto/PageRequestDTO.java
@@ -1,4 +1,29 @@
 package org.news.itword.dto;
 
+import lombok.*;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@Builder
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 public class PageRequestDTO {
+
+    private int page;
+    private int size;
+    private int keyword;
+    private int category;
+
+    public PageRequestDTO() {
+        this.page = 1;
+        this.size = 10;
+    }
+
+    public Pageable getPageable(Sort sort) {
+        return PageRequest.of(page - 1, size, sort);
+    }
+
 }
