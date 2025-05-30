f3c37547a224db1e63bee0ed1376eaf255a50bfa
bydhorizon0 <bydhorizon0@gmail.com>
2025-05-30 17:38:34 +0900

    findById() 메서드 제거

--- src/main/java/org/news/itword/repository/MemberRepository.java
+++ src/main/java/org/news/itword/repository/MemberRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findById(long id);
-
     @EntityGraph(attributePaths = "roles", type = EntityGraph.EntityGraphType.LOAD)
     @Query("SELECT m FROM Member m WHERE m.email = :email")
     Optional<Member> findByEmail(String email);
