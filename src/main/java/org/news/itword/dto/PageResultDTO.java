53c80fa2cfac4341cd5a73e51cf77f49cb85ad9c
bydhorizon0 <bydhorizon0@gmail.com>
2025-06-06 14:40:37 +0900

    PageResultDTO 작성 대기

--- src/main/java/org/news/itword/dto/PageResultDTO.java
+++ src/main/java/org/news/itword/dto/PageResultDTO.java
@@ -1,4 +1,28 @@
 package org.news.itword.dto;
 
-public class PageResultDTO {
+import lombok.*;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class PageResultDTO<DTO> {
+
+    private int page;
+    private int size;
+    private int totalPage;
+
+    private int start, end;
+    private boolean prev, next;
+
+    private List<Integer> pageList;
+    private List<DTO> dtoList;
+
+    public PageResultDTO(Page<DTO> result) {
+
+    }
 }
