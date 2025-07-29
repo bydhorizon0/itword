3fa88800f70bb520c958c1bedd1e241bf2ff5972
kihyeonyang <khyang@bizdata.kr>
2025-07-29 10:13:33 +0900

    정적 리소스 매핑 경로 수정

--- src/main/java/org/news/itword/config/WebConfig.java
+++ src/main/java/org/news/itword/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("images/**")
-                .addResourceLocations("classpath:/static/images/");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:images/");
     }
 }
